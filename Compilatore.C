51c3ba5869da846f659df959e939b1b6431aac09
Ludovico Massaccesi <62757383+lmassach@users.noreply.github.com>
2025-11-06 11:10:46 +0100

    Style fix

--- Compilatore.C
+++ Compilatore.C
@@ -1,33 +1,32 @@
 //{
-#include <Riostream.h>
-#include <TROOT.h>
-#include <TObject.h>
-#include <TSystem.h>
-#include "TStopwatch.h"
-#include "TMath.h"
 #include "TAxis.h"
+#include "TCanvas.h"
 #include "TFile.h"
 #include "TH1.h"
 #include "TH1D.h"
-#include "TCanvas.h"
-#include <TTree.h>
-#include <TBranch.h>
-#include <TSystemDirectory.h>
-#include <TSystemFile.h>
-#include <TList.h>
-#include "include/general.hpp"
+#include "TMath.h"
+#include "TStopwatch.h"
 #include "include/Chameleon.h"
 #include "include/ConfigFile.hpp"
+#include "include/general.hpp"
 #include "src/Analyzer.hpp"
+#include <Riostream.h>
+#include <TBranch.h>
+#include <TList.h>
+#include <TObject.h>
+#include <TROOT.h>
+#include <TSystem.h>
+#include <TSystemDirectory.h>
+#include <TSystemFile.h>
+#include <TTree.h>
 
-void Compilatore(){
+void Compilatore() {
 
-  gSystem->CompileMacro("src/general.cpp","kg");
-  gSystem->CompileMacro("src/Chameleon.cpp","kg");
-  gSystem->CompileMacro("src/ConfigFile.cpp","kg");
-  gSystem->CompileMacro("src/Analyzer.cpp","kg");
-  gSystem->CompileMacro("analisi.C","kg");
+  gSystem->CompileMacro("src/general.cpp", "kg");
+  gSystem->CompileMacro("src/Chameleon.cpp", "kg");
+  gSystem->CompileMacro("src/ConfigFile.cpp", "kg");
+  gSystem->CompileMacro("src/Analyzer.cpp", "kg");
+  gSystem->CompileMacro("analisi.C", "kg");
 
-  gROOT->ProcessLine( "analisi()" );
+  gROOT->ProcessLine("analisi()");
 }
-
